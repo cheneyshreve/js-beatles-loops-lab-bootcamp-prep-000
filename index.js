8303a892d5f7986d164d558d506f89ec98a415c9
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-26 20:11:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ function iLoveTheBeatles(n){
  var myArray = []
  var i = 0;
  do {
-  
-   return myArray
+ myArray.push("I love the Beatles!")
+ return myArray
  } while (i < n || i < 15)
-  myArray.push("I love the Beatles!")
   i++
 }

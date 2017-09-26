0752fd18bd10140ca608e770a8acf4294b9fd35f
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-26 19:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
  for (i = 0; i < musicians.length; i++){
    var array = []
-   array.push(`${musicians[i]}` + "plays " + `${instruments[i]}`)
+   array.push(`${musicians[i]}` + " plays " + `${instruments[i]}`)
  }
  return array
 }
